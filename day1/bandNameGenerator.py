0032ee9274473608745f30bedba502740e8ae15d
Zach Riane Machacon <zachrianemachacon@yahoo.com>
2022-06-10 00:35:53 +0800

    Finished Day 1

--- day1/bandNameGenerator.py
+++ day1/bandNameGenerator.py
@@ -1,7 +1,5 @@
 print("Welcome to the Band Name Generator")
-print("What's the name of the city you grew up in?")
-cityInput = input()
-print("What's your pet's name?")
-petInput = input()
+cityInput = input("What's the name of the city you grew up in?\n")
+petInput = input("What's your pet's name?\n")
 bandName = cityInput + " " + petInput
 print(f"Your band name could be {bandName}")
