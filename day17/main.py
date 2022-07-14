f6aff19a9704ce745c27bcc2aef17c8f926d60f6
Zach Riane Machacon <zachrianemachacon@yahoo.com>
2022-07-14 16:55:52 +0800

    Completed Day 17 (Trivia Quiz App with Open Trivia API)

---
+++ day17/main.py
@@ -0,0 +1,19 @@
+# Main
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = list()
+
+for questions in question_data:
+    new_q = Question(questions["text"], questions["answer"])
+    question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("The quiz is complete!")
+print(f"Your final score is: {quiz.score}/{len(quiz.question_list)}")
